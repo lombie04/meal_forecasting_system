760f4e86298cb0ba8f2517761703eede1e04288a
Salem Lombard <salemlombard3@gmail.com>
2025-05-06 19:52:29 +0200

    Fix missing comma between 'humanize' and 'system' in INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
     'system',
 ]
 
